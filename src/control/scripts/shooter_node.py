37a413fb6c411a828e98eef7e74c4632a901242b
Ole Berger <ole.k.berger@t-online.de>
2019-08-08 20:51:46 +0100

    Add reset service for shooter node.

--- src/control/scripts/shooter_node.py
+++ src/control/scripts/shooter_node.py
@@ -4,17 +4,31 @@ import rospy
 import dynamic_reconfigure.server
 from control.cfg import ShooterConfig
 from perception_msgs.msg import FaceDetectionStamped
+import std_srvs.srv
 
 NODE_NAME = "shooter_node"
 
 class ShooterNode:
     def __init__(self):
-        self.face_detection_sub = rospy.Subscriber("{}/face_detection".format(NODE_NAME), FaceDetectionStamped, self.handle_face_detection)
         self.reconfigure_server = dynamic_reconfigure.server.Server(ShooterConfig, self.handle_reconfigure)
+        self.reset_server = rospy.Service("{}/reset".format(NODE_NAME), std_srvs.srv.Empty, self.handle_reset)
+        self.face_detection_sub = rospy.Subscriber("{}/face_detection".format(NODE_NAME), FaceDetectionStamped, self.handle_face_detection)
+        self.config = None
 
     def handle_reconfigure(self, config, level):
-        rospy.loginfo("Reconfigured P: {}, D: {}".format(config.aim_p, config.aim_d))
+        rospy.loginfo("Reconfigured.")
+        # Update config and reset.
+        self.config = config
+        self.reset()
         return config
+
+    def handle_reset(self, req):
+        self.reset()
+        return std_srvs.srv.EmptyResponse()
+
+    def reset(self):
+        rospy.loginfo("Resetting.")
+        # TODO: Reset state etc.
 
     def handle_face_detection(self, det_msg):
         rospy.loginfo(rospy.get_caller_id() + ": Received detection.")
