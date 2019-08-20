18ab161e00c7be4c03529ae027f07fe1ab7e0a1e
Ole Berger <ole.k.berger@t-online.de>
2019-08-20 18:47:00 +0100

    Comment out prints.

--- src/control/scripts/aimer.py
+++ src/control/scripts/aimer.py
@@ -21,6 +21,7 @@ class Aimer:
     def set_default_orientation(self):
         self.set_pan(90)
         self.set_tilt(90)
+        rospy.loginfo("Set initial orientation.")
 
     def set_parameters(self, pan_p, pan_d, tilt_p, tilt_d):
         self.pan_p = pan_p
@@ -35,13 +36,13 @@ class Aimer:
         clipped_new_pan = np.clip(new_pan, 60, 120)
         self.pan_servo.angle = clipped_new_pan
         self.pan = clipped_new_pan
-        rospy.loginfo("Pan: {}".format(self.pan_servo.angle))
+        #rospy.loginfo("Pan: {}".format(self.pan_servo.angle))
 
     def set_tilt(self, new_tilt):
         clipped_new_tilt = np.clip(new_tilt, 80, 140)
         self.tilt_servo.angle = clipped_new_tilt
         self.tilt = clipped_new_tilt
-        rospy.loginfo("Tilt: {}".format(self.tilt_servo.angle))
+        #rospy.loginfo("Tilt: {}".format(self.tilt_servo.angle))
 
     def aim(self, target):
         # PD control
@@ -51,7 +52,7 @@ class Aimer:
         if self.previous_error is not None:
             velocity += np.array([self.pan_d, self.tilt_d]) * (error - self.previous_error)
         self.previous_error = error
-        rospy.loginfo("Velocity: {}, {}".format(velocity[0], velocity[1]))
+        #rospy.loginfo("Velocity: {}, {}".format(velocity[0], velocity[1]))
         # Apply
         self.set_pan(self.pan + velocity[0])
         self.set_tilt(self.tilt + velocity[1])
