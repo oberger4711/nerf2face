aa027ee353b9cd2e7b97acfd9ba8fc3bd4ece847
Ole Berger <ole.k.berger@t-online.de>
2019-10-23 21:00:15 +0100

    Implement state controlling and shutdown via button.

--- src/nerf2face/scripts/nerf2face_node.py
+++ src/nerf2face/scripts/nerf2face_node.py
@@ -12,7 +12,9 @@ import signal
 
 NODE_NAME = "nerf2face_node"
 
-GPIO_PIN_STATE_BUTTON = 4
+GPIO_PIN_IN_STATE_BUTTON = 4
+GPIO_PIN_OUT_PARKED_LED = 5
+GPIO_PIN_OUT_SHOOTING_LED = 6
 
 def call_empty_service(service_path):
     rospy.wait_for_service(service_path)
@@ -34,18 +36,26 @@ def reset_face_tracker():
 class Nerf2FaceNode:
     def __init__(self):
         self.state = "PARKED"
-        self.enter_parked()
-        self.state_button = gpiozero.Button(GPIO_PIN_STATE_BUTTON, hold_time=2)
+        self.parked_led = gpiozero.LED(GPIO_PIN_OUT_PARKED_LED)
+        self.shooting_led = gpiozero.LED(GPIO_PIN_OUT_SHOOTING_LED)
+        self.state_button = gpiozero.Button(GPIO_PIN_IN_STATE_BUTTON, pull_up=False, hold_time=3)
         self.state_button.when_pressed = self.toggle_state
         self.state_button.when_held = self.shutdown
+        self.enter_parked()
     
     def enter_shooting(self):
+        rospy.loginfo("Switching to SHOOTING state.")
         self.state = "SHOOTING"
+        self.parked_led.off()
+        self.shooting_led.on()
         reset_shooter()
         reset_face_tracker()
 
     def enter_parked(self):
+        rospy.loginfo("Switching to PARKED state.")
         self.state = "PARKED"
+        self.shooting_led.off()
+        self.parked_led.on()
         park_shooter()
         reset_face_tracker()
 
@@ -53,10 +63,12 @@ class Nerf2FaceNode:
         if self.state == "PARKED":
             self.enter_shooting()
         else:
-            self.enter_enter_parked()
+            self.enter_parked()
 
     def shutdown(self):
         rospy.loginfo("Shutting down...")
+        self.parked_led.off()
+        self.shooting_led.off()
         time.sleep(0.3)
         subprocess.check_call(["sudo", "poweroff"])
 
