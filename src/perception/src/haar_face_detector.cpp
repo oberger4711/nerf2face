a7e31d6e07b578b7216b131918710878d88c87e7
Ole Berger <ole.k.berger@t-online.de>
2023-04-26 22:39:34 +0100

    Downscale image before detection

--- src/perception/src/haar_face_detector.cpp
+++ src/perception/src/haar_face_detector.cpp
@@ -1,9 +1,21 @@
 #include "haar_face_detector.h"
 
+#include <algorithm>
+
+constexpr int DOWN_SCALING_FACTOR = 2;
+
 HaarFaceDetector::HaarFaceDetector() {
     detector.load(ros::package::getPath("perception") + "/resources/haarcascade_frontalface_default.xml");
 }
 
 void HaarFaceDetector::detect(const cv::Mat& img_gray, std::vector<cv::Rect>& face_detections) {
-    detector.detectMultiScale(img_gray, face_detections, 1.1, 4, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
+    cv::Mat img_gray_s;
+    cv::resize(img_gray, img_gray_s, cv::Size(), 1.0 / DOWN_SCALING_FACTOR, 1.0 / DOWN_SCALING_FACTOR, cv::INTER_LINEAR);
+    detector.detectMultiScale(img_gray_s, face_detections, 1.1, 4, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(30 / DOWN_SCALING_FACTOR, 30 / DOWN_SCALING_FACTOR));
+    for (auto& det : face_detections) {
+        det.x = det.x * DOWN_SCALING_FACTOR;
+        det.y = det.y * DOWN_SCALING_FACTOR;
+        det.width = det.width * DOWN_SCALING_FACTOR;
+        det.height = det.height * DOWN_SCALING_FACTOR;
+    }
 }
